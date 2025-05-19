2aa59d5dcf3f1495396c66f9e1402bc7d10a205d
garamzui <forsave121@gmail.com>
2025-05-19 11:50:25 +0900

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -104,6 +104,24 @@ public class PlayerController : MonoBehaviour
             }
         }
         return false;
+       
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Ray[] rays = new Ray[4]
+        {
+        new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+        new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+        new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+        new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            Gizmos.DrawLine(rays[i].origin, rays[i].origin + rays[i].direction * 0.1f);
+        }
     }
 }
 
